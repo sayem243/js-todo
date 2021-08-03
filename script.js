01b475caa4ae840543fe8db7d8bd97c9c246882a
sayem243 <sayem243@gmail.com>
2021-08-04 02:06:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+/*
+ * Title: To Do Application using vanilla JS DOM
+ * Description: This JS file has all the JS functions necessary to control the to do application
+ * Author: Md Sayem ( Learn from Sumit vai )
+ * Date: 4/8/2021
+ *
+ */
+
+// select elements & assign them to variables
+
+
+
+
+
+
+
+ // select elements & assign them to variables
+ let newTask = document.querySelector('#new-task');
+ let form = document.querySelector('form');
+ let todoUl = document.querySelector('#items');
+ let completeUl = document.querySelector('.complete-list ul');
+
+
+// functions
+let createTask = function(task) {
+    let listItem = document.createElement('li');
+    let checkBox = document.createElement('input');
+    let label = document.createElement('label');
+
+    label.innerText = task;
+    checkBox.type = 'checkbox';
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    return listItem;
+}
+
+let addTask = function(event) {
+    event.preventDefault();
+    let listItem = createTask(newTask.value);
+    todoUl.appendChild(listItem);
+    newTask.value = "";
+    // bind the new list item to the incomplete list
+    bindInCompleteItems(listItem, completeTask);
+}
+
+let completeTask = function() {
+    let listItem = this.parentNode;
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.className = 'delete';
+    listItem.appendChild(deleteBtn);
+
+    let checkBox = listItem.querySelector('input[type="checkbox"]');
+    checkBox.remove();
+    completeUl.appendChild(listItem);
+    bindCompleteItems(listItem, deleteTask);
+}
+
+let deleteTask = function() {
+    let listItem = this.parentNode;
+    let ul = listItem.parentNode;
+    ul.removeChild(listItem);
+}
+
+let bindInCompleteItems = function(taskItem, checkboxClick) {
+    let checkBox = taskItem.querySelector('input[type="checkbox"]');
+    checkBox.onchange = checkboxClick;
+}
+
+let bindCompleteItems = function(taskItem, deleteButtonClick) {
+    let deleteButton = taskItem.querySelector('.delete');
+    deleteButton.onclick = deleteButtonClick;
+}
+
+for(let i=0; i< todoUl.children.length; i++ ) {
+    bindInCompleteItems(todoUl.children[i], completeTask);
+}
+
+for(let i=0; i< completeUl.children.length; i++ ) {
+    bindCompleteItems(completeUl.children[i], deleteTask);
+}
+
+form.addEventListener('submit', addTask);
+
+
+// console.log(document.URL)
+// console.log(document.getElementById('new-task'))
+// console.log(document.getElementById('new-task'))
+
+// let lastItems=document.querySelector('.item:nth-child(2)')
+
+// lastItems.style.color="red";
+
+// const parent =document.querySelector('#items')
+// const children=parent.children;
+// console.log(children);
+// //children[0].innerHTML="Hi";
+// // children.textContent="oka"
+
+// const headerElement=document.querySelector('#header')
+
+// const newok =headerElement.addEventListener('click',(event)=>{
+    
+// })
